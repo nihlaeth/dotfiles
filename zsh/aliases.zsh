780e633829f980054cc4f49279cfec107b1c6fda
nihlaeth <info@nihlaeth.nl>
2016-08-13 16:55:32 +0200

    extra aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
 alias reload!='. ~/.zshrc'
+
+# speech recognition aliases
+alias e='vim'
+alias add='git add'
+alias commit='git commit'
+alias status='git status'
+
+# package manager aliases
+alias spu='slackpkg -batch=on -default_answer=n update; slackpkg upgrade-all'
